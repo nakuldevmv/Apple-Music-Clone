77019d8db1e97a3f9e148bc53240e2e252f7d74e
Nakul Dev <nakuldev1561@gmail.com>
2024-03-11 22:57:08 +0530

    Started Creating Browse Page

--- lib/pages/browse_page.dart
+++ lib/pages/browse_page.dart
@@ -8,13 +8,51 @@ class BrowsePage extends StatefulWidget {
 }
 
 class _BrowsePageState extends State<BrowsePage> {
+  List Banner = [
+    {
+      't1': 'Womens History Month',
+      't2': 'Alpha',
+      't3': 'Alpha Music',
+      'img': 'assets/1.jpg',
+    }
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Container(
-        child: Text("Browse Page"),
-      )),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        toolbarHeight: 100,
+        title: Text(
+          "Browse",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+        ),
+        actions: [
+          Container(
+            // color: Colors.amber,
+            padding: EdgeInsets.only(right: 5),
+            child: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.account_circle_rounded,
+                  size: 40,
+                )),
+          )
+        ],
+      ),
+      body: Container(
+        height: 250,
+        width: 300,
+        color: const Color.fromARGB(255, 80, 71, 37),
+        child: Column(
+          children: [
+            Text("Womens History Month"),
+            Image.asset(Banner[0]['img']),
+            Text("data"),
+            Text("data"),
+          ],
+        ),
+      ),
     );
   }
 }
