ac7f30b2539d0ae34b4d83b2894579ea99f2bf99
Nakul Dev <nakuldev1561@gmail.com>
2024-03-11 23:30:37 +0530

    added top banner

--- lib/pages/browse_page.dart
+++ lib/pages/browse_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class BrowsePage extends StatefulWidget {
   const BrowsePage({super.key});
@@ -8,12 +9,36 @@ class BrowsePage extends StatefulWidget {
 }
 
 class _BrowsePageState extends State<BrowsePage> {
-  List Banner = [
+  List ban = [
     {
-      't1': 'Womens History Month',
+      't1': 'WOMENS HISTORY MONTH',
       't2': 'Alpha',
-      't3': 'Alpha Music',
+      't3': 'Apple Music',
       'img': 'assets/1.jpg',
+    },
+    {
+      't1': 'NEW PLAYLIST',
+      't2': 'Reception Hits',
+      't3': 'Apple Music',
+      'img': 'assets/2.jpg',
+    },
+    {
+      't1': 'JUST UPDATED',
+      't2': 'Soundtrack your wedding with songs',
+      't3': 'Apple Music',
+      'img': 'assets/3.jpg',
+    },
+    {
+      't1': 'ADD TO YOUR LIBRARY',
+      't2': 'Soul Revival',
+      't3': 'Apple Music R&B',
+      'img': 'assets/4.jpg',
+    },
+    {
+      't1': 'NEW PLAYLIST',
+      't2': 'Punjabi Workout',
+      't3': 'Apple Music Punjabi',
+      'img': 'assets/5.jpg',
     }
   ];
   @override
@@ -41,18 +66,62 @@ class _BrowsePageState extends State<BrowsePage> {
         ],
       ),
       body: Container(
-        height: 250,
-        width: 300,
-        color: const Color.fromARGB(255, 80, 71, 37),
-        child: Column(
-          children: [
-            Text("Womens History Month"),
-            Image.asset(Banner[0]['img']),
-            Text("data"),
-            Text("data"),
-          ],
-        ),
-      ),
+          padding: EdgeInsets.only(top: 10, left: 0, right: 0),
+          height: 350,
+          // width: 1080 / 2.5,
+          // color: const Color.fromARGB(255, 80, 71, 37),
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: ban.length,
+            itemBuilder: (context, index) {
+              return Container(
+                // color: Colors.red,
+                margin: EdgeInsets.only(left: 6, right: 6),
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        ban[index]['t1'],
+                        style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                      ),
+                      Text(
+                        ban[index]['t2'],
+                        style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.white),
+                      ),
+                      Text(
+                        ban[index]['t3'],
+                        style: TextStyle(
+                            fontSize: 19,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.grey),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      ClipPath(
+                          clipper: ShapeBorderClipper(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(13)),
+                          ),
+                          child: Image.asset(
+                            ban[index]['img'],
+                            height: 720 / 3,
+                            width: 1080 / 3,
+                            fit: BoxFit.cover,
+                          )),
+                    ],
+                  ),
+                ),
+              );
+            },
+          )),
     );
   }
 }
