a63d2648cd9cc03286a693a839215034db5931b5
Nakul Dev <nakuldev1561@gmail.com>
2024-03-13 23:09:59 +0530

    working with radio.dart

--- lib/pages/radio.dart
+++ lib/pages/radio.dart
@@ -8,6 +8,50 @@ class RadioPage extends StatefulWidget {
 }
 
 class _RadioPageState extends State<RadioPage> {
+  List TopPicks = [
+    {
+      //here i mentioned the color in hexadecimal format should start with 0xFF
+      "topMsg": "Made for you",
+      "image":
+          "https://upload.wikimedia.org/wikipedia/en/1/1b/Joji_-_Nectar.png",
+      "title": "Nectar",
+      "artist": "Joji",
+      "song": ""
+    },
+    {
+      "topMsg": "New Release",
+      "image":
+          "https://static.stereogum.com/uploads/2023/03/LDR-Tunnel-1679672318-1000x1000.jpg",
+      "title": "Ocean Blvd",
+      "artist": "Lana Del Rey",
+      "song": ""
+    },
+    {
+      "topMsg": "Featuring Tame Impala",
+      "image":
+          "https://qodeinteractive.com/magazine/wp-content/uploads/2020/06/16-Tame-Impala.jpg",
+      "title": "Tame Impala",
+      "artist": "Currents",
+      "song": ""
+    },
+    {
+      "topMsg": "Made for you",
+      "image":
+          "https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcT9kry1myZTi2dMJ7OtgJjmdT__lImpI-pJ9mdq42Cz8HhIet_ro_Obp6q4xbksBbpT",
+      "title": "The dark side of the moon",
+      "artist": "Pink floyd ",
+      "song": ""
+    },
+    // {
+    //   "topMsg": "Featuring Tame Nico",
+    //   "image":
+    //       "https://upload.wikimedia.org/wikipedia/en/0/0c/Velvet_Underground_and_Nico.jpg",
+    //   "title": "The Velvet Underground and Nico",
+    //   "artist": "Nico",
+    //   "song": ""
+    // },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +75,151 @@ class _RadioPageState extends State<RadioPage> {
                 )),
           )
         ],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Column(
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: 10),
+                              // padding: EdgeInsets.all(5),
+                              child: Icon(
+                                Icons.apple,
+                                size: 25,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Container(
+                                margin: EdgeInsets.only(left: 1),
+                                child: Text("Music 1",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold))),
+                          ],
+                        ),
+                        Container(
+                            margin: EdgeInsets.only(left: 10),
+                            child: Text("The new music matter",
+                                style: TextStyle(
+                                    color: Colors.grey, fontSize: 15))),
+                      ],
+                    ),
+                    Icon(Icons.calendar_month_rounded),
+                  ],
+                ),
+                Container(
+                  // color: Colors.amber,
+                  margin: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipPath(
+                        clipper: ShapeBorderClipper(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(15),
+                                topRight: Radius.circular(15)),
+                          ),
+                        ),
+                        child: Image.network(
+                          TopPicks[0]['image'],
+                          height: 280,
+                          width: 380,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      ClipPath(
+                        clipper: ShapeBorderClipper(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(15),
+                                bottomRight: Radius.circular(15)),
+                          ),
+                        ),
+                        child: Container(
+                          padding: EdgeInsets.only(left: 10, right: 10),
+                          height: 100,
+                          width: 380,
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                            colors: [
+                              const Color.fromARGB(255, 83, 83, 83),
+                              Color.fromARGB(255, 65, 65, 65)
+                            ],
+                            stops: [0, 1],
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                          )),
+
+                          // color: Color.fromARGB(255, 255, 227, 114),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text("LIVE • 9:30 - 11:30 PM",
+                                            style: TextStyle(
+                                                color: Colors.grey,
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.bold)),
+                                        Text(
+                                          TopPicks[0]['title'],
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                          ),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        Text(
+                                          TopPicks[0]['artist'],
+                                          style: TextStyle(
+                                              color: Colors.grey, fontSize: 18),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ],
+                                    ),
+                                    Icon(
+                                      Icons.play_circle_fill_rounded,
+                                      size: 35,
+                                      color: Colors.white,
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
