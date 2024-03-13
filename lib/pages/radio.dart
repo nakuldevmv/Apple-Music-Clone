8d80413258a3a1cc7beebbc74051df1d94817991
Nakul Dev <nakuldev1561@gmail.com>
2024-03-13 22:24:04 +0530

    moved list.dart & Created radio page

---
+++ lib/pages/radio.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RadioPage extends StatefulWidget {
+  const RadioPage({super.key});
+
+  @override
+  State<RadioPage> createState() => _RadioPageState();
+}
+
+class _RadioPageState extends State<RadioPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
