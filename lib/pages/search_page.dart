5d644464880e638100106d5cffebcf8128cd4aa0
Nakul Dev <nakuldev1561@gmail.com>
2024-03-15 23:10:31 +0530

    Update

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -166,7 +166,7 @@ class _SearchPageState extends State<SearchPage> {
       backgroundColor: Color.fromARGB(255, 0, 0, 0),
       appBar: AppBar(
         backgroundColor: Colors.black,
-        toolbarHeight: 100,
+        toolbarHeight: 1000,
         title: Text(
           "Search",
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
