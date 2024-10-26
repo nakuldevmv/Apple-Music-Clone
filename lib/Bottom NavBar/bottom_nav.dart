7897aaff013b2baa25f6b0c04e6dc5c3f797141d
Nakul Dev <nakuldev1561@gmail.com>
2024-10-26 21:58:58 +0530

    Update bottom_nav.dart

--- lib/Bottom NavBar/bottom_nav.dart
+++ lib/Bottom NavBar/bottom_nav.dart
@@ -43,72 +43,57 @@ class _FirstPageState extends State<FirstPage> {
 
         // padding: EdgeInsets.only(bottom: 0),
         // color: Colors.amber,
-        child: GlassFlexContainer(
-          borderRadius: BorderRadius.zero,
-          // borderRadius: BorderRadius.only(
-          //   topLeft: Radius.circular(15),
-          // //   topRight: Radius.circular(15),
-          // // ),
-          borderGradient: const LinearGradient(
-            colors: [
-              Color.fromARGB(0, 11, 11, 11)
-            ], // Add this line to remove border stroke
-            stops: [
-              0
-            ],
+        child: Theme(
+          data: Theme.of(context).copyWith(
+            // Apply transparent color to both splashColor and highlightColor
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
           ),
-          child: Theme(
-            data: Theme.of(context).copyWith(
-              // Apply transparent color to both splashColor and highlightColor
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
-            ),
-            child: BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              currentIndex: _selected_index,
-              onTap: _navgateBottomBar,
-              items: [
-                const BottomNavigationBarItem(
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: _selected_index,
+            onTap: _navgateBottomBar,
+            items: [
+              const BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home_filled,
+                  size: 35,
+                ),
+                label: 'Home',
+              ),
+              const BottomNavigationBarItem(
                   icon: Icon(
-                    Icons.home_filled,
+                    Icons.grid_view_rounded,
                     size: 35,
                   ),
-                  label: 'Home',
-                ),
-                const BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.grid_view_rounded,
-                      size: 35,
-                    ),
-                    label: 'Browse'),
-                const BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.sensors,
-                      size: 35,
-                    ),
-                    label: 'Radio'),
-                const BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.subscriptions_rounded,
-                      size: 35,
-                    ),
-                    label: 'Library'),
-                const BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.search,
-                      size: 35,
-                    ),
-                    label: 'Search'),
-              ],
-              elevation: 0,
-              selectedLabelStyle: const TextStyle(color: Colors.white),
-              selectedItemColor: const Color.fromARGB(255, 255, 17, 0),
-              unselectedItemColor: Colors.grey,
-              showUnselectedLabels: true,
-              backgroundColor: Colors.transparent,
-              unselectedFontSize: 13.0,
-              selectedFontSize: 13.0,
-            ),
+                  label: 'Browse'),
+              const BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.sensors,
+                    size: 35,
+                  ),
+                  label: 'Radio'),
+              const BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.subscriptions_rounded,
+                    size: 35,
+                  ),
+                  label: 'Library'),
+              const BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.search,
+                    size: 35,
+                  ),
+                  label: 'Search'),
+            ],
+            elevation: 0,
+            selectedLabelStyle: const TextStyle(color: Colors.white),
+            selectedItemColor: const Color.fromARGB(255, 255, 17, 0),
+            unselectedItemColor: Colors.grey,
+            showUnselectedLabels: true,
+            backgroundColor: Colors.transparent,
+            unselectedFontSize: 13.0,
+            selectedFontSize: 13.0,
           ),
         ),
       ),
